97f45da0926b2d42e12f61c673f5ec214b5bacc1
Fede Gómez <gomez.fede98@gmail.com>
2022-08-08 14:01:53 -0400

    Extracted icons in separate components

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import CategoryCard from './UI/CategoryCard';
+import CategoryCard from './CategoryCard';
 import MenFashion from '../public/mens-clothes.jpg';
 import WomenFashion from '../public/women-clothes.jpg';
 import Link from 'next/link';
 
 const Categories = () => {
 	return (
-		<div className='container mx-auto px-4 flex items-center justify-center flex-col md:flex-row'>
+		<div className='container mx-auto mb-32 px-4 flex items-center justify-center flex-col md:flex-row'>
 			<Link href='/mens'>
 				<CategoryCard image={MenFashion} title='Hombres' />
 			</Link>
