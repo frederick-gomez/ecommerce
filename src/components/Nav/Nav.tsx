9fb29145fa6c8f103a46b3a9ad1b65e8bbcc2dea
Fede Gómez <gomez.fede98@gmail.com>
2022-08-22 20:56:09 -0400

    Added about page

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React, { useState } from 'react';
 import Menu from './Menu';
 
@@ -5,16 +6,18 @@ const Nav = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 	return (
-		<nav className='bg-stone-100 shadow dark:bg-neutral-900 fixed z-[100] w-full'>
-			<div className='container px-6 py-4 mx-auto md:flex md:justify-between md:items-center'>
+		<nav className='fixed z-[100] w-full bg-stone-100 shadow dark:bg-neutral-900'>
+			<div className='container mx-auto px-6 py-4 md:flex md:items-center md:justify-between'>
 				<div className='flex items-center justify-between'>
 					<div>
-						<a
-							className='text-2xl font-bold text-gray-900 dark:text-gray-200 transform lg:text-3xl hover-link'
-							href='#'
-						>
-							E-commerce
-						</a>
+						<Link href='/'>
+							<a
+								className='hover-link transform text-2xl font-bold text-gray-900 dark:text-gray-200 lg:text-3xl'
+								href='#'
+							>
+								E-commerce
+							</a>
+						</Link>
 					</div>
 
 					{/* Mobile menu button */}
@@ -22,10 +25,10 @@ const Nav = () => {
 						<button
 							type='button'
 							onClick={() => setIsMenuOpen(!isMenuOpen)}
-							className='hover-link text-gray-900 dark:text-gray-200 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400'
+							className='hover-link text-gray-900 focus:text-gray-600 focus:outline-none dark:text-gray-200 dark:focus:text-gray-400'
 							aria-label='toggle menu'
 						>
-							<svg viewBox='0 0 24 24' className='w-6 h-6 fill-current'>
+							<svg viewBox='0 0 24 24' className='h-6 w-6 fill-current'>
 								<path
 									fillRule='evenodd'
 									d='M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z'
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import Image from 'next/image';
+import About1 from '../../public/about-1.jpg';
 
 type Props = {};
 
 const AboutPage = (props: Props) => {
-	return <div>About</div>;
+	return (
+		<section className='container mx-auto px-4 pt-16'>
+			<div className='md: items-center py-6 md:flex'>
+				<div className='md:basis-1/2 md:px-6'>
+					<h1 className='py-2 text-xl font-semibold'>Sobre Nosotros</h1>
+					<p className='py-2 text-sm'>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime veniam quisquam sequi
+						velit, quas aspernatur aperiam id non? Harum, doloribus. Lorem, ipsum dolor sit amet
+						consectetur adipisicing elit. Magni quia, ex ullam nostrum ipsum, corrupti ipsa ab, est
+						culpa repellendus dolore voluptatum harum obcaecati in officia soluta voluptatibus
+						aliquam tempora!
+					</p>
+					<p className='py-2 text-sm'>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis perspiciatis suscipit
+						laudantium non voluptas labore blanditiis expedita esse rerum sapiente!
+					</p>
+				</div>
+				<div className='basis-1/2 py-4 md:w-full md:grow'>
+					<Image src={About1} layout='responsive' />
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default AboutPage;
