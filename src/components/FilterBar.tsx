7eb5e1a15c4748048a709b3b579f28929681f4ca
Fede Gómez <gomez.fede98@gmail.com>
2022-09-05 16:00:39 -0400

    Removed unused import

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 import ChevronDownSVG from '../components/icons/ChevronDownSVG';
 import TrashSVG from './icons/TrashSVG';
 
